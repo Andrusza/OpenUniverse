bd192514cdc78b892f831ae8af39add752b5a410
Andrew <andrusza1989@gmail.com>
2012-10-01 11:12:56 +0200

    added glm

---
+++ OpenUniverse/basic.h
@@ -0,0 +1,14 @@
+// Include Windows functions
+#include <Windows.h>
+
+// Include input and output operations
+#include <iostream>
+
+// OpenGL and GLEW Header Files and Libraries
+#include "GL/glew.h"
+#include "GL/freeglut.h"
+
+
+// GLM include files
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp" 
